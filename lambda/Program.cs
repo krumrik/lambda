3a5779cd25b912b3b922be102b37da17354a8c99
kayde <kayde@KAYDENS-PC>
2024-03-31 03:08:36 -0400

    Add project files.

---
+++ lambda/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LambdaCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Func<double, double, double> add = (x, y) => x + y;
+            Func<double, double, double> multiply = (x, y) => x * y;
+            Func<double, double, double> smallerValue = (x, y) => x < y ? x : y;
+
+            Console.WriteLine($"{num1} + {num2} = {add(num1, num2)}");
+            Console.WriteLine($"{num1} * {num2} = {multiply(num1, num2)}");
+            Console.WriteLine($"{num1} - {num2} = {smallerValue(num1, num2)}");
+
+        }
+    }
+}
+
